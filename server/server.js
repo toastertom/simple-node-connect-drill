3a050a5546ae0cf8433d93ce327546d565039a2b
ToasterTom <tate.thomas5595@gmail.com>
2016-10-22 09:39:30 -0600

    first commit

--- server/server.js
+++ server/server.js
@@ -4,15 +4,21 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var chatCtrl = require('./controllers/chatCtrl');
 var port = 3737;
-
+var config = require('./server_config');
 // TODO server front end files with static
 
 app.use(bodyParser.json());
-
+app.use(express.static(__dirname + '/public'));
 // TODO Initialize Session
+app.use(session({
+  secret: config.sessionSecret,
+  saveUninitialized: false,
+  resave: false
+}));
 
-app.post("/api/screenname", function(req, res){
+app.post("/api/:screenname", function(req, res){
   // TODO Save screenname to session
+  req.session.screenname = req.params.screenname;
 })
 
 app.get("/api/chats", chatCtrl.getChats);
@@ -22,5 +28,5 @@ app.delete("/api/chats", chatCtrl.deleteChats);
 
 
 app.listen(port, function(){
-  console.log("Listeing on port ", port, " INSERT-WITTY-STATEMENT-HERE");
+  console.log("Listeing on port ", port, " All your base belong to us!");
 });
